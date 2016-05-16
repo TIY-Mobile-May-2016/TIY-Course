566da60c1fa8d12ec7aafc9d1b13baf4c26683ba
Phil Wright <touchopia@me.com>
2016-05-16 10:30:14 -0600

    Adding Prework

---
+++ 00--Developers-Awaken/CounterApp-Starter/CounterApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CounterApp
+//
+//  Created by Phil Wright on 5/16/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
