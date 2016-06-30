4dc920172e91dc56b4219885beefdad9a6583243
Phil Wright <touchopia@me.com>
2016-06-30 09:55:04 -0600

    Adding calendarview

--- 29-Cocoapods/SimpleCalendarDemo/SimpleCalendarDemo/ViewController.swift
+++ 29-Cocoapods/SimpleCalendarDemo/SimpleCalendarDemo/ViewController.swift
@@ -7,19 +7,17 @@
 //
 
 import UIKit
+import CalendarView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let calendar = CalendarView(frame: CGRectMake(0, 0, CGRectGetWidth(view.frame), 320))
+        view.addSubview(calendar)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
